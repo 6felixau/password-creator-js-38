7eec8654681761fb7fb8abd300ac8fe3be41cb82
frontend-analyst-65 <6@felixau.in>
2026-07-25 15:10:44 +0530

    Implement features in pass_gen.js - Version 6

--- pass_gen.js
+++ pass_gen.js
@@ -5,3 +5,5 @@ function generatePassword(length = 12) {
     let password = "";
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
